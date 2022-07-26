8799864068a9bc0b6e6a9883ad615dbb8bfe33c0
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-26 15:17:49 -0300

    Dividindo uma string e transformando em um array

--- src/challenges.js
+++ src/challenges.js
@@ -27,10 +27,14 @@ console.log(calcArea(51, 1));
 
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+    let array = string.split(" ");
+    return array;
 }
-
+  console.log(splitSentence('go Trybe'));
+  console.log(splitSentence('vamo que vamo'));
+  console.log(splitSentence('foguete'));
+  
 
 
 // Desafio 4
