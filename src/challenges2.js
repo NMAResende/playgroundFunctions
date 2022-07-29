4191c9dc80557315c6e0717f75d98a134126d2b5
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-28 22:56:11 -0300

    Realizando o desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,39 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumbers) {
+
+arrayNumbers.splice(0, 0, '(');
+arrayNumbers.splice(3, 0, ')');
+arrayNumbers.splice(4, 0, ' ');
+arrayNumbers.splice(10, 0, '-');
+let texto =  arrayNumbers.join('');
+
+  if (arrayNumbers.length > 11) {
+    return 'Array com tamanho incorreto.'
+  }
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    if (arrayNumbers[i] < 0 || arrayNumbers[i] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores'
+    }
+  }
+  let cont = 0;
+  let novoArray = arrayNumbers.sort(function (a, b) { return a - b });
+  let numeroRepetido = novoArray[0];
+  for (let i = 0; i < novoArray.length; i += 1) {
+    if (novoArray[i] == numeroRepetido) {
+      cont = cont + 1;
+      if (cont > 2) {
+        return 'não é possível gerar um número de telefone com esses valores'
+      }
+    } else {
+      numeroRepetido = novoArray[i]
+      cont = 1;
+    }
+  }
+
 }
+
+console.log(generatePhoneNumber([1,2,3,4,5,6,7,8,9,0,1]))
+
 
 // Desafio 12
 function triangleCheck() {
