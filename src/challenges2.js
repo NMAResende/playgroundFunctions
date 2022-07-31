e1e2445daa069f1430362a09a35ad7868a77ad2f
Nathália <nathaliamarciaandrade@gmail.com>
2022-07-31 11:07:23 -0300

    Refatorando desafio

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,12 +1,11 @@
 // Desafio 11
 function generatePhoneNumber(arrayNumbers) {
   if (arrayNumbers.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  }
+    return 'Array com tamanho incorreto.'};
   for (let i = 0; i < arrayNumbers.length; i += 1) {
     if (arrayNumbers[i] < 0 || arrayNumbers[i] > 9) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
   }
   let cont = 0;
   let novoArray = [];
@@ -16,12 +15,10 @@ function generatePhoneNumber(arrayNumbers) {
   novoArray = novoArray.sort(function (a, b) { return a - b });
   let numeroRepetido = novoArray[0];
   for (let i = 0; i < novoArray.length; i += 1) {
-    if (novoArray[i] === numeroRepetido) {
-      cont += 1;
+    if (novoArray[i] === numeroRepetido) cont += 1;
       if (cont > 2) {
-        return 'não é possível gerar um número de telefone com esses valores';
-      }
-    } else {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }else {
       numeroRepetido = novoArray[i];
       cont = 1;
     }
@@ -36,14 +33,29 @@ function generatePhoneNumber(arrayNumbers) {
 console.log(generatePhoneNumber([0, 2, 3, 4, 4, 2, 7, 8, 9, 9, 4]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  let valorAbsA = Math.abs(lineB % lineB);
+  let valorAbsB = Math.abs(lineA % lineC);
+  let valorAbsC = Math.abs(lineA % lineB);
+  if (lineA < lineB + lineC || lineB < lineA + lineC || lineC < lineA + lineB && lineA > valorAbsA || lineB > valorAbsB || lineC > valorAbsC){
+    return true;
+  } 
+  return false;
 }
+console.log(triangleCheck(10, 14, 8));
+console.log(triangleCheck(28,14,8));
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+let matches = string.match(/\d+/g);
+let sum = 0;
+  for (let i = 0; i < matches.length; i += 1){
+    let n1 = Number(matches[i].value);
+    sum = sum + Number[i];
 }
+return sum + " copos de água";
+}
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 
 module.exports = {
   generatePhoneNumber,
